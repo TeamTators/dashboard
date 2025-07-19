c416764447ff87e89a1e56b9b855b886013c84f8
Taylor King <taylorking206@u.boisestate.edu>
2025-07-18 22:00:12 -0600

    merge main - there are issues with this one

--- src/routes/dashboard/mentor/+layout@.svelte
+++ src/routes/dashboard/mentor/+layout@.svelte
@@ -2,8 +2,8 @@
 	import Navbar from '$lib/components/general/Navbar.svelte';
 	import Footer from '$lib/components/general/Footer.svelte';
 	import { PUBLIC_APP_NAME } from '$env/static/public';
-    import nav from '$lib/imports/mentor';
-    nav();
+	import nav from '$lib/imports/mentor';
+	nav();
 
 	const { children } = $props();
 </script>
