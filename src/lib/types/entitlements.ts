0636e983e46b76b85e857c3f0ec5bb9256654be4
Taylor King <taylorking206@u.boisestate.edu>
2025-07-19 11:30:44 -0600

    entitlements

--- src/lib/types/entitlements.ts
+++ src/lib/types/entitlements.ts
@@ -1,22 +1,7 @@
 export type Entitlement = 
     'view-roles'
-  | 'view-strategy'
-  | 'view-checklist'
-  | 'edit-potato-level'
-  | 'manage-tba'
   | 'create-custom-tba-responses'
-  | 'view-potatoes'
-  | 'upload-pictures'
-  | 'view-pit-scouting'
-  | 'manage-scouting'
-  | 'manage-pit-scouting'
-  | 'view-tba-info'
-  | 'view-scouting';
+  | 'manage-tba';
 	export type Group = 
     'Roles'
-  | 'Strategy'
-  | 'Checklists'
-  | 'Potatoes'
-  | 'TBA'
-  | 'FIRST'
-  | 'Scouting';
+  | 'TBA';
