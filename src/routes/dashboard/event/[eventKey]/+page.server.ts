24086a72434a8481eb7488e30a92edec5f2016a5
taylor <taylor.king206@u.boisestate.edu>
2025-08-25 20:57:18 -0600

    init

--- src/routes/dashboard/event/[eventKey]/+page.server.ts
+++ src/routes/dashboard/event/[eventKey]/+page.server.ts
@@ -12,7 +12,8 @@ export const load = async (event) => {
 	if (!event.locals.account) throw redirect(ServerCode.temporaryRedirect, '/account/sign-in');
 	const e = await TBA.Event.getEvent(event.params.eventKey);
 	if (e.isErr()) {
-		throw redirect(ServerCode.temporaryRedirect, `/404?url${event.url.href}`);
+		// throw redirect(ServerCode.temporaryRedirect, `/404?${event.url.href}`);
+		throw fail(404); // SOPHIE, YOU SHOULD DO THIS
 	}
 
 	const [teams, matches] = await Promise.all([e.value.getTeams(), e.value.getMatches()]);
