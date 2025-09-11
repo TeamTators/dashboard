ddb89b1ef8855a7f7800644628be76c08eb964c9
taylor <taylor.king206@u.boisestate.edu>
2025-09-11 00:58:58 -0600

    format and health check

--- src/lib/types/entitlements.ts
+++ src/lib/types/entitlements.ts
@@ -1,15 +1,7 @@
-export type Entitlement =
-	| 'view-roles'
-	| 'view-pit-scouting'
-	| 'manage-pit-scouting'
-	| 'create-custom-tba-responses'
-	| 'view-strategy'
-	| 'edit-potato-level'
-	| 'view-potatoes'
-	| 'manage-tba'
-	| 'manage-scouting'
-	| 'view-tba-info'
-	| 'view-checklist'
-	| 'view-scouting'
-	| 'upload-pictures';
-export type Group = 'Roles' | 'Scouting' | 'TBA' | 'Strategy' | 'Potatoes' | 'FIRST' | 'Checklists';
+export type Entitlement = 
+    'view-roles'
+  | 'create-custom-tba-responses'
+  | 'manage-tba';
+	export type Group = 
+    'Roles'
+  | 'TBA';
