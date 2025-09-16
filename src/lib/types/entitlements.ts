acb29730c71ec1e65584ed2bcda18a277c98dc57
Ansodo-SophieH <ansodohung@gmail.com>
2025-09-15 21:23:57 -0600

    add stash

--- src/lib/types/entitlements.ts
+++ src/lib/types/entitlements.ts
@@ -1,28 +1,22 @@
-export type Entitlement =
-	| 'manage-pit-scouting'
-	| 'view-potatoes'
-	| 'manage-scouting'
-	| 'view-strategy'
-	| 'edit-potato-level'
-	| 'manage-tba'
-	| 'create-custom-tba-responses'
-	| 'view-tba-info'
-	| 'view-roles'
-	| 'view-checklist'
-	| 'upload-pictures'
-	| 'view-scouting'
-	| 'view-pit-scouting'
-	| 'manage-roles'
-	| 'manage-members'
-	| 'test-permission-view'
-	| 'test-permission-manage';
-export type Group =
-	| 'Scouting'
-	| 'Potatoes'
-	| 'Strategy'
-	| 'TBA'
-	| 'FIRST'
-	| 'Roles'
-	| 'Checklists'
-	| 'Testing';
-export type Features = 'manage-roles';
+export type Entitlement = 
+    'view-roles'
+  | 'view-potatoes'
+  | 'edit-potato-level'
+  | 'view-strategy'
+  | 'create-custom-tba-responses'
+  | 'manage-tba'
+  | 'view-checklist'
+  | 'view-tba-info'
+  | 'upload-pictures'
+  | 'manage-pit-scouting'
+  | 'manage-scouting'
+  | 'view-scouting'
+  | 'view-pit-scouting';
+	export type Group = 
+    'Roles'
+  | 'Potatoes'
+  | 'Strategy'
+  | 'TBA'
+  | 'Checklists'
+  | 'FIRST'
+  | 'Scouting';
