721aca8b20d00faea9c1104e6beb98727183547f
Ansodo-SophieH <ansodohung@gmail.com>
2025-09-24 23:18:34 -0600

    switched to ag-grid

--- src/routes/dashboard/event/[eventKey]/checklist/+page.svelte
+++ src/routes/dashboard/event/[eventKey]/checklist/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import Grid from '$lib/components/general/Grid.svelte';
 	import nav from '$lib/imports/robot-display';
+	import { readable } from 'svelte/store';
+	import { NumberFilterModule, TextFilterModule } from 'ag-grid-community';
+	
 	const { data } = $props();
 	const event = $derived(data.event);
-	const checklist = $derived(data.data);
+	const checklist = $derived(readable(data.data));
 
 	$effect(() => nav(event.tba));
+	
 </script>
 
 <div class="container">
@@ -16,46 +21,59 @@
 	<div class="row mb-3">
 		<div class="col">
 			<h4>Pictures</h4>
+			<Grid
+				rowNumbers={true}
+				modules={[NumberFilterModule, TextFilterModule]}
+				opts={{
+					columnDefs: [
+						{
+							headerName: 'Team',
+							field: 'team.tba.team_number',
+							filter: 'agTextColumnFilter'
+						},
+						{
+							headerName: 'TBA',
+							field: 'tbaPictures',
+							filter: 'agNumberColumnFilter'
+						},
+						{
+							headerName: 'Uploaded',
+							field: 'uploaded',
+							filter: 'agNumberColumnFilter'
+						}
+					]
+				}}
+				data={checklist}
+				height={400}
+			/>
 		</div>
-	</div>
-	<div class="row mb-3">
-		{#each checklist as item}
-			<div class="col">
-				<h4 class:text-danger={item.tbaPictures + item.uploaded === 0}>
-					{item.team.tba.team_number}
-					{item.team.tba.nickname}
-				</h4>
-				<ul class="list">
-					<li class="list-group-item">
-						TBA: {item.tbaPictures}
-					</li>
-					<li class="list-group-item">
-						Uploaded: {item.uploaded}
-					</li>
-				</ul>
-			</div>
-		{/each}
-	</div>
-	<hr />
-	<div class="row mb-3">
 		<div class="col">
 			<h4>Pit Scouting</h4>
+			<Grid
+				rowNumbers={true}
+				modules={[NumberFilterModule, TextFilterModule]}
+				opts={{
+					columnDefs: [
+						{
+							headerName: 'Team',
+							field: 'team.tba.team_number',
+							filter: 'agTextColumnFilter'
+						},
+						{
+							headerName: 'TBA',
+							field: 'tbaPictures',
+							filter: 'agNumberColumnFilter'
+						},
+						{
+							headerName: 'Uploaded',
+							field: 'uploaded',
+							filter: 'agNumberColumnFilter'
+						}
+					]
+				}}
+				data={checklist}
+				height={400}
+			/>
 		</div>
-	</div>
-	<div class="row mb-3">
-		{#each checklist as item}
-			{#if item.left.data.length}
-				<div class="col">
-					<h4>{item.team.tba.team_number} {item.team.tba.nickname} ({item.left.data.length})</h4>
-					<ul class="list-group">
-						{#each item.left.data as question}
-							<li class="list-group text-danger">
-								<pre>{question.data.key}</pre>
-							</li>
-						{/each}
-					</ul>
-				</div>
-			{/if}
-		{/each}
 	</div>
 </div>
