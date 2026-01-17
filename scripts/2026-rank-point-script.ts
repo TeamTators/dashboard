a8412d7f3227d1677138ab9058e1cba3896b51c3
tsaxking <taylorking206@u.boisestate.edu>
2026-01-17 21:29:58 +0000

    climb data

--- scripts/2026-rank-point-script.ts
+++ scripts/2026-rank-point-script.ts
@@ -31,41 +31,101 @@ export default async (eventKey: string, year: string) => {
 
     const rankPointCount = async (team: Team) => {
         const events = await Event.getTeamEvents(targetYear, team.tba.team_number).unwrap();
-        let rp1 = 0;
-        let rp2 = 0;
-        let rp3 = 0;
-        let totalmatches = 0;
+        // let rp1 = 0;
+        // let rp2 = 0;
+        // let rp3 = 0;
+        // let totalmatches = 0;
+        let climbData = 0;
         let eventExist = true;
         let alliance: 'blue' | 'red' | null = null;
 
-        for (let j=0; j<events.length; j++) {
-            const newTeam = new Team(team.tba, events[j]);
-            const m = await newTeam.getMatches().unwrap();
-            
-            for (let i=0; i<m.length; i++) {
-                const res = m[i].asYear(2025); //2025 is supposed to be targetYear but asYear doesn't support other years yet
-                if (!res.isOk()) {eventExist = false; continue;}
-                const matches = res.value;
-                if (!matches.score_breakdown) continue;
-
-                if (matches.alliances.blue.team_keys.includes(team.tba.key)) {alliance = 'blue';} 
-                    else if (matches.alliances.red.team_keys.includes(team.tba.key)) {alliance = 'red';} 
-                    else {console.log("No alliance found");}
-                if (!alliance) continue;
-
-                if (matches.score_breakdown[alliance].autoBonusAchieved) rp1++;
-                if (matches.score_breakdown[alliance].coralBonusAchieved) rp2++;
-                if (matches.score_breakdown[alliance].bargeBonusAchieved) rp3++;
-                alliance = null;
+        const [firstEvent] = events;
+        if (!firstEvent) {
+            return {
+                team: team.tba.team_number,
+                name: team.tba.nickname,
+                data: {
+                    // autoBonus: rp1,
+                    // coralBonus: rp2,
+                    // bargeBonus: rp3,
+                    // totalmatches,
+                    climb: climbData,
+                }
             }
-            if (!eventExist) {eventExist = true; continue;}
-            totalmatches += m.length;
         }
-        return ["Team Number: " + String(team.tba.team_number) + " " + team.tba.nickname, "total", ["autoBonus", rp1, "coralBonus", rp2, "bargeBonus", rp3, "totalMatches", totalmatches]];
+
+        
+        const newTeam = new Team(team.tba, firstEvent);
+        const m = await newTeam.getMatches().unwrap();
+        
+        for (let i=0; i<m.length; i++) {
+            const res = m[i].asYear(2025); //2025 is supposed to be targetYear but asYear doesn't support other years yet
+            if (!res.isOk()) {eventExist = false; continue;}
+            const matches = res.value;
+            if (!matches.score_breakdown) continue;
+
+            if (matches.alliances.blue.team_keys.includes(team.tba.key)) {alliance = 'blue';} 
+                else if (matches.alliances.red.team_keys.includes(team.tba.key)) {alliance = 'red';} 
+                else {console.log("No alliance found");}
+            if (!alliance) continue;
+
+            const teamPos = matches.alliances[alliance].team_keys.indexOf(team.tba.key);
+
+            console.log(matches.score_breakdown[alliance]);
+
+            if (teamPos === 0) {
+                climbData += ['DeepCage', 'ShallowCage'].includes(matches.score_breakdown[alliance].endGameRobot1) ? 1 : 0;
+            } else if (teamPos === 1) {
+                climbData += ['DeepCage', 'ShallowCage'].includes(matches.score_breakdown[alliance].endGameRobot2) ? 1 : 0;
+            } else if (teamPos === 2) {
+                climbData += ['DeepCage', 'ShallowCage'].includes(matches.score_breakdown[alliance].endGameRobot3) ? 1 : 0;
+            }
+
+            // if (matches.score_breakdown[alliance].autoBonusAchieved) rp1++;
+            // if (matches.score_breakdown[alliance].coralBonusAchieved) rp2++;
+            // if (matches.score_breakdown[alliance].bargeBonusAchieved) rp3++;
+            alliance = null;
+        }
+
+        // for (let j=0; j<events.length; j++) {
+        //     const newTeam = new Team(team.tba, events[j]);
+        //     const m = await newTeam.getMatches().unwrap();
+            
+        //     for (let i=0; i<m.length; i++) {
+        //         const res = m[i].asYear(2025); //2025 is supposed to be targetYear but asYear doesn't support other years yet
+        //         if (!res.isOk()) {eventExist = false; continue;}
+        //         const matches = res.value;
+        //         if (!matches.score_breakdown) continue;
+
+        //         if (matches.alliances.blue.team_keys.includes(team.tba.key)) {alliance = 'blue';} 
+        //             else if (matches.alliances.red.team_keys.includes(team.tba.key)) {alliance = 'red';} 
+        //             else {console.log("No alliance found");}
+        //         if (!alliance) continue;
+
+        //         if (matches.score_breakdown[alliance].autoBonusAchieved) rp1++;
+        //         if (matches.score_breakdown[alliance].coralBonusAchieved) rp2++;
+        //         if (matches.score_breakdown[alliance].bargeBonusAchieved) rp3++;
+        //         alliance = null;
+        //     }
+        //     if (!eventExist) {eventExist = true; continue;}
+        //     totalmatches += m.length;
+        // }
+        // return ["Team Number: " + String(team.tba.team_number) + " " + team.tba.nickname, "total", ["autoBonus", rp1, "coralBonus", rp2, "bargeBonus", rp3, "totalMatches", totalmatches]];
+        return {
+            team: team.tba.team_number,
+            name: team.tba.nickname,
+            data: {
+                climb: climbData,
+                // autoBonus: rp1,
+                // coralBonus: rp2,
+                // bargeBonus: rp3,
+                // totalmatches,
+            }
+        }
     };
 
     const results = await Promise.all(teams.map(rankPointAvg));
     const rankedResults = await Promise.all(teams.map(rankPointCount));
     console.log("Rank Point Average:", results);
-    console.log("Rank Point Count:", rankedResults);
+    console.log("Rank Point Count:", rankedResults.sort((a, b) => a.data.climb - b.data.climb));
 };
