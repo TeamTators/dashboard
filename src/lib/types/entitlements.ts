8a54e194965decf949e4d8cf24f40f0b5decad33
LenekyRadish <29kynlee.cook@boiseschools.net>
2026-01-20 03:11:35 +0000

    init

--- src/lib/types/entitlements.ts
+++ src/lib/types/entitlements.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export type Entitlement =
 	| 'view-roles'
 	| 'view-tba-info'
@@ -25,3 +26,35 @@ export type Group =
 	| 'Checklists'
 	| 'Testing';
 export type Features = 'manage-potatoes' | 'manage-roles';
+=======
+export type Entitlement = 
+    'view-pit-scouting'
+  | 'create-custom-tba-responses'
+  | 'view-roles'
+  | 'test-permission-manage'
+  | 'manage-tba'
+  | 'view-scouting'
+  | 'manage-pit-scouting'
+  | 'manage-scouting'
+  | 'manage-members'
+  | 'test-permission-view'
+  | 'manage-roles'
+  | 'view-checklist'
+  | 'upload-pictures'
+  | 'view-potatoes'
+  | 'edit-potato-level'
+  | 'view-strategy'
+  | 'view-tba-info';
+export type Group = 
+    'Scouting'
+  | 'TBA'
+  | 'Roles'
+  | 'Testing'
+  | 'Checklists'
+  | 'FIRST'
+  | 'Potatoes'
+  | 'Strategy';
+export type Features = 
+	'manage-roles'
+  | 'manage-potatoes';
+>>>>>>> Stashed changes
