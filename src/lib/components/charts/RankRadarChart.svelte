605174f5d09c2aa2abf049e6aab28ce1eb0f46f6
tsaxking <taylorking206@u.boisestate.edu>
2026-01-21 04:00:10 +0000

    for landon

---
+++ src/lib/components/charts/RankRadarChart.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    /* 
+        Create a radar chart of what rank that team has in various metrics.
+        The further out the point is, the better the rank (1 is best, so it should be at the edge of the chart).
+    
+        View ./RadarChart.svelte to see how to create a radar chart.
+    */
+
+    /*
+        /src/routes/dashboard/event/[eventKey]/+page.svelte
+
+        FIRST.getSummary(event.tba.key, event.tba.year as 2024 | 2025, {
+        // 10 minutes
+        cacheExpires: after(10 * 60 * 1000)
+    }).then((res) => {
+        if (res.isOk()) {
+            // you will need to edit this line
+            summary = res.value.pivot().teamsRanked();
+        } else {
+            console.error('Error fetching event summary:', res.error);
+        }
+    });
+    */
+</script>
