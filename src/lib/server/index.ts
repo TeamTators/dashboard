e17a4f6f055de71d57b3820e9bbe9423343d1862
tsaxking <taylorking206@u.boisestate.edu>
2026-01-23 04:14:30 +0000

    fix hash issue

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -6,19 +6,21 @@ import { config } from './utils/env';
 
 testSchema('false');
 export const postBuild = async () => {
-try {
+	try {
 		const lifetimeLoop = Struct.generateLifetimeLoop(
 			1000 * 60 * 60 * 24 * 7 // 1 week
 		);
 		lifetimeLoop.start();
-	
+
 		const ADMIN_USERNAME = config.admin.user;
 		const ADMIN_EMAIL = config.admin.email;
 		const ADMIN_PASSWORD = config.admin.pass;
 		const ADMIN_FIRST_NAME = config.admin.first_name;
 		const ADMIN_LAST_NAME = config.admin.last_name;
-	
-		const admin = await Account.Account.fromProperty('username', ADMIN_USERNAME, { type: 'single' });
+
+		const admin = await Account.Account.fromProperty('username', ADMIN_USERNAME, {
+			type: 'single'
+		});
 		if (admin.isErr()) {
 			terminal.error(`Failed to find admin account: ${admin.error}`);
 			return;
@@ -37,14 +39,14 @@ try {
 					canUpdate: false
 				}
 			).unwrap();
-	
+
 			await res
 				.update({
 					verified: true,
 					verification: ''
 				})
 				.unwrap();
-	
+
 			await Account.Admins.new(
 				{
 					accountId: res.id
@@ -54,9 +56,9 @@ try {
 				}
 			).unwrap();
 		}
-} catch (error) {
-	terminal.warn(error);
-}
+	} catch (error) {
+		terminal.warn(error);
+	}
 };
 
 {
