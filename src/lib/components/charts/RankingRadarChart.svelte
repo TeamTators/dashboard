97afb9b25cf0a8b499e445730859b4c8535cf0af
code-control1 <lpanic2020@gmail.com>
2026-01-28 01:26:23 +0000

    Create Radar Ranking chart

---
+++ src/lib/components/charts/RankingRadarChart.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import RadarChart from "./RadarChart.svelte";
+	import { TBATeam } from '$lib/utils/tba';
+
+	interface Props {
+		team: TBATeam;
+		eventSummary: unknown;
+	}
+
+	const { team, eventSummary }: Props = $props();
+</script>
+<RadarChart 
+	team={}
+	data={{}}
+/>
