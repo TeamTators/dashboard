99f75b8c9eee33a7e7a5864750838dfa4fdaaff1
Monkeymoose159 <danielvstead@outlook.com>
2026-02-11 04:02:46 +0000

    init new function

--- src/lib/utils/trace/summaries/2026.ts
+++ src/lib/utils/trace/summaries/2026.ts
@@ -1,12 +1,47 @@
 import YearInfo2026 from 'tatorscout/years/2026.js';
 import { Aggregators } from 'tatorscout/summary';
 
+const summariesViaTBA = (data, dataType, param: string) => {
+	const summaryData = data.matches.map(m => {
+				const parsed = YearInfo2026.parseMatch(m).unwrap();
+				const redPosition = m.alliances.red.team_keys.indexOf('frc' + data.team);
+				const bluePosition = m.alliances.blue.team_keys.indexOf('frc' + data.team);
+				const alliance = redPosition !== -1 ? 'red' : bluePosition !== -1 ? 'blue' : null;
+				const position = alliance === 'red' ? redPosition : alliance === 'blue' ? bluePosition : -1;
+				if (alliance && position !== -1) {
+					dataType = [
+						parsed.score_breakdown[alliance][param + 1],
+						parsed.score_breakdown[alliance][],
+						parsed.score_breakdown[alliance].autoTowerRobot3
+					][position];
+
+					// const endClimb = [
+					// 	parsed.score_breakdown[alliance].endgameTowerRobot1,
+					// 	parsed.score_breakdown[alliance].endgameTowerRobot2,
+					// 	parsed.score_breakdown[alliance].endgameTowerRobot3
+					// ][position];
+
+					let points = 0;
+					// if (dataType !== 'None') points += 15; -- ************Specific************
+
+					// const end = {
+					// 	'Level1': 10,
+					// 	'Level2': 20,
+					// 	'Level3': 30
+					// };
+					// if (endClimb in end) points += end[endClimb as keyof typeof end];
+					return points;
+				}
+				return 0;
+			});
+			return Aggregators.average(summaryData);
+		}
+
+
+
 export default YearInfo2026.summary({
 	'Auto Points': {
-		'Average Hub Scored': (data) => {
-			const hubData = data.scoring.map((d) => d.auto.hub1 + d.auto.hub5 + d.auto.hub10);
-			return Aggregators.average(hubData);
-		},
+		'Average Hub Scored': (data) => { return Aggregators.average(data.scoring.map((d) => d.auto.hub1 + d.auto.hub5 + d.auto.hub10)) },
 		'Average Climb': (data) => {
 			const climbData = data.matches.map(m => {
 				const parsed = YearInfo2026.parseMatch(m).unwrap();
