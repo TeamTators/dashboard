b9406562aa7e5771ffc1705b517b19a2bb4029d1
tsaxking <taylorking206@u.boisestate.edu>
2026-02-28 20:24:58 +0000

    fix sumbit match noncompressed

--- src/routes/event-server/submit-match/compressed/+server.ts
+++ src/routes/event-server/submit-match/compressed/+server.ts
@@ -10,12 +10,11 @@ import terminal from '$lib/server/utils/terminal';
 import { Trace } from 'tatorscout/trace';
 import { ServerCode } from 'ts-utils/status';
 import { Account } from '$lib/server/structs/account';
-import { Err, resolveAll } from 'ts-utils/check';
+import { Err, resolveAll, Ok } from 'ts-utils/check';
 import { Logs } from '$lib/server/structs/log.js';
 import { str } from '$lib/server/utils/env.js';
 import { decompress } from '$lib/server/utils/compression';
 import type { RequestEvent } from './$types';
-import { Ok } from 'ts-utils';
 
 /**
  * Handles compressed match submissions from the event server.
@@ -197,8 +196,7 @@ export const POST = async (event: RequestEvent) => {
 
 	const commentRes = resolveAll(
 		await Promise.all(
-			Object.entries(comments).map(([key, value]) =>
-			{
+			Object.entries(comments).map(([key, value]) => {
 				if (value.trim() === '') return Promise.resolve(new Ok('Empty comment'));
 				return Scouting.TeamComments.new({
 					accountId,
@@ -221,9 +219,8 @@ export const POST = async (event: RequestEvent) => {
 						terminal.error('Error creating comments', res.error);
 						return new Err(res.error);
 					}
-				})
-			}
-			)
+				});
+			})
 		)
 	);
 
